68fbdfed05e6a0b50c6a66e6d9abaecbd36395c9
slbn <santiago.bldn@gmail.com>
2024-05-15 00:24:29 +0200

    git

--- ADCMPC3002.py
+++ ADCMPC3002.py
@@ -4,130 +4,55 @@
 # from raspi-config. The example is based on a SparkFun tutorial:
 # https://learn.sparkfun.com/tutorials/python-programming-tutorial-getting-started-with-the-raspberry-pi/experiment-3-spi-and-analog-input
 
-import signal
+#!/usr/bin/python3
+
 import sys
-import time
 import datetime
 import spidev
-import RPi.GPIO as GPIO
-import json
-import os
 
+class SensorReader:
+    def __init__(self, spi_channel=0):
+        self.spi = spidev.SpiDev()
+        self.spi.open(0, spi_channel)
+        self.spi.max_speed_hz = 1200000
 
+    def close(self):
+        self.spi.close()
+        sys.exit(0)
 
-spi_ch = 0
+    def read_adc(self, channel):
+        if channel != 0:
+            channel = 1
+        msg = 0b11
+        msg = ((msg << 1) + channel) << 5
+        msg = [msg, 0b00000000]
+        reply = self.spi.xfer2(msg)
+        adc = 0
+        for n in reply:
+            adc = (adc << 8) + n
+        adc = adc >> 1
+        return adc
 
-# Enable SPI
-spi = spidev.SpiDev(0, spi_ch)
-spi.max_speed_hz = 1200000
+    def read_temperature(self):
+        adc = self.read_adc(0)
+        voltage = (adc * 5) / 1023
+        temperature = ((voltage * 5 / 1023) - 1.51) * 17 / 1.01
+        return temperature
 
-def close(signal, frame):
-    sys.exit(0)
+    def read_luminosity(self):
+        adc = self.read_adc(1)
+        return adc
 
-signal.signal(signal.SIGINT, close)
+    def read_average_adc(self, channel, samples=10):
+        total = sum(self.read_adc(channel) for _ in range(samples))
+        return total / samples
 
-def get_adc(channel):
-
-    # Make sure ADC channel is 0 or 1
-    if channel != 0:
-        channel = 1
-
-    # Construct SPI message
-    #  First bit (Start): Logic high (1)
-    #  Second bit (SGL/DIFF): 1 to select single mode
-    #  Third bit (ODD/SIGN): Select channel (0 or 1)
-    #  Fourth bit (MSFB): 0 for LSB first
-    #  Next 12 bits: 0 (don't care)
-    msg = 0b11
-    msg = ((msg << 1) + channel) << 5
-    msg = [msg, 0b00000000]
-    reply = spi.xfer2(msg)
-
-    # Construct single integer out of the reply (2 bytes)
-    adc = 0
-    for n in reply:
-        adc = (adc << 8) + n
-
-    # Last bit (0) is not part of ADC value, shift to remove it
-    adc = adc >> 1
-    
-    # Calculate voltage form ADC value
-    # considering the soil moisture sensor is working at 5V
-    # print("adc directo "+str(adc)+" "+str((5 * adc) / 1023))
-    # voltage = (5 * adc) / 1023
-
-    return adc
-
-def get_temp(channel):
-    if channel != 0:
-        channel = 1
-    # Construct SPI message
-    #  First bit (Start): Logic high (1)
-    #  Second bit (SGL/DIFF): 1 to select single mode
-    #  Third bit (ODD/SIGN): Select channel (0 or 1)
-    #  Fourth bit (MSFB): 0 for LSB first
-    #  Next 12 bits: 0 (don't care)
-    msg = 0b11
-    msg = ((msg << 1) + channel) << 5
-    msg = [msg, 0b00000000]
-    reply = spi.xfer2(msg)
-
-    # Construct single integer out of the reply (2 bytes)
-    adc = 0
-    for n in reply:
-        adc = (adc << 8) + n
-
-    # Last bit (0) is not part of ADC value, shift to remove it
-    adc = adc >> 1
-
-    # Calculate voltage form ADC value
-    # considering the soil moisture sensor is working at 5V
-
-    return ((adc*5/1023)-(1.51))*17/1.01
-
-def adcAvr(channel):
-    #Channel 0 Temperature
-    #Channel 1 Luminocity
-    avr=0
-
-    if channel != 0:
-        for i in range(10):
-            avr+=get_adc(1)
-            time.sleep(0.2)
-    else:
-        for i in range(10):
-            avr+=get_temp(0)
-            time.sleep(0.2)
-
-
-    return round(avr/10)
-
-
-def getValues():
-    dt=datetime.datetime.now()
-    datos = {
-    "timestamp":dt,
-    "temperature": adcAvr(0),
-    "luminocity":adcAvr(1)
-    # "luminosidad": luminosidad
-    }
-    return datos
-    ruta_archivo = '/home/pi/Desktop/test.json'
-    with open(ruta_archivo, 'w') as archivo_json:
-        json.dump(datos, archivo_json)
-    try:
-        with open(ruta_archivo, 'w') as archivo_json:
-            json.dump(datos, archivo_json)
-    except Exception as e:
-        print(f"¡Error! No se pudo crear el archivo JSON: {e}")
-
-
-#Function for testing porpouses
-def printValues():
-    while True:
-       
-        print("Luminocity "+str(adcAvr(1)))
-        print("Time stamp "+str(time.time()))
-        print("Temperature "+str(adcAvr(0)))
-        print("------------------")
-        time.sleep(2)
+    def get_values(self):
+        timestamp = datetime.datetime.now()
+        temperature = self.read_average_adc(0)
+        luminosity = self.read_average_adc(1)
+        return {
+            "Date": timestamp.strftime("%Y-%m-%d %H:%M:%S"),
+            "Temperature": temperature,
+            "Light": luminosity
+        }
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import ADCMPC3002
+import signal
+import json
+import time
 
 
-def main():
-    
-    # ADCMPC3002.getValues()
-    print((ADCMPC3002.getValues()))
+def signal_handler(sig, frame):
+    sensor.close()
 
+sensor=ADCMPC3002.SensorReader()
+signal.signal(signal.SIGINT, signal_handler)
 
 if __name__ == "__main__":
-    main()
+    try:
+        while True:
+            data = sensor.get_values()
+            print(json.dumps(data, indent=4))
+            time.sleep(1)  # Ajusta según sea necesario
+    except KeyboardInterrupt:
+        sensor.close()
